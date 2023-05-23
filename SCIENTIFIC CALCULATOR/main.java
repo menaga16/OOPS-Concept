d880e31186a33a39ace407263cf68f5de7b322bd
Menaga Raman <116909288+menaga16@users.noreply.github.com>
2023-05-23 23:42:33 +0530

    Add files via upload

---
+++ SCIENTIFIC CALCULATOR/main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sci_Calculator;
+
+/**
+ *
+ * @author menaga
+ */
+
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            
+Calculator cal = new Calculator();
+
+    }
+    
+}
