0c95c5af30a33867563fcf724918eecd69eefc8b
Menaga Raman <116909288+menaga16@users.noreply.github.com>
2023-05-23 23:45:45 +0530

    Update main.java

--- SCIENTIFIC CALCULATOR/main.java
+++ SCIENTIFIC CALCULATOR/main.java
@@ -1,19 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package sci_Calculator;
 
-/**
- *
- * @author menaga
- */
+package sci_Calculator;
 
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
             
 Calculator cal = new Calculator();
