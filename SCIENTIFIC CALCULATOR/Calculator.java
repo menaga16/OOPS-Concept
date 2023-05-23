2220cc57bc32abc155680d44b3d0624cf7141292
Menaga Raman <116909288+menaga16@users.noreply.github.com>
2023-05-23 23:45:12 +0530

    Update Calculator.java

--- SCIENTIFIC CALCULATOR/Calculator.java
+++ SCIENTIFIC CALCULATOR/Calculator.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sci_Calculator;
-
 import java.awt.Color;
 import java.awt.*;
 import javax.swing.*;
-
-/**
- *
- * @author Hossa
- */
 public class Calculator {
-    
-    
+      
      JFrame frame;
     JLabel label = new JLabel();
     JTextField textField = new JTextField(" 0 ");
